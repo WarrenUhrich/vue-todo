da873fc83e92d5e3cc092dd8ce7ef0bc51f64e5c
Warren Uhrich <hello@warren.codes>
2020-04-16 16:39:31 -0600

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Second message!"/>
+    <HelloWorld msg="Third message!!!"/>
   </div>
 </template>
 
